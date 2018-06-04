af0835f4f8cec0e036d088a2e4e581a03992dc1e
Judge Gambill <judgegambill@gmail.com>
2018-06-04 22:08:44 +0200

    First Implementation

--- src/contacts/Main.java
+++ src/contacts/Main.java
@@ -1,10 +1,16 @@
 package contacts;
 
+import contacts.ui.TextUserInterface;
+import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
         // Start your program here
         // ATTENTION: In your program, you can create only one instance of class Scanner!
+        Scanner reader = new Scanner(System.in);
+        TextUserInterface ui = new TextUserInterface(reader);
+        ui.start();
     }
 }
